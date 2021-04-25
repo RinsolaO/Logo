8d9c4f4f1aa337c4a1b9631b6c7d17c8ed7b5ad8
RinsolaO <feranmiayomide@gmail.com>
2021-04-25 14:41:52 +0100

    Updated Menu Bar, So when Highlighed Opacity Increases

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -10,32 +10,45 @@ import earn from './img/earn.png'
 
 const Menu = () => {
 
-    const 
+    const [activeClass, setActiveClass ] = useState('home');
 
     return (
         <div className = 'menu-flex'>
-            <div className="menu-box">
-            <img src={home} alt="home"/>
-            <h5 onClick = ""> <Link to ="/">Home</Link> </h5>
+            <div id="home">
+                <div className={`menu-box ${activeClass === 'home'? 'menu-box-active' : ''} `}>
+                <img src={home} alt="home"/>
+                <h5 onClick = {() => setActiveClass('home')}> <Link to ="/">Home</Link> </h5>
+                </div>
             </div>
-            <div className="menu-box">
-            <img src={activity} alt="activity"/>
-            <h5> <Link to="/Activity">Activity</Link> </h5>
-  
+            <div id="activity">
+                <div className={`menu-box ${activeClass === 'activity' ? 'menu-box-active' : ''} `}>
+                <img src={activity} alt="activity"/>
+                <h5 onClick = {() => setActiveClass('activity')}> <Link to="/Activity">Activity</Link> </h5>
+                </div> 
             </div>
-            <div className="menu-box">
-            <img src={wallet} alt=""/>
-            <h5> <Link to="/Wallet">Wallet</Link> </h5>
-          
+            
+            <div id="wallet">
+                <div className={`menu-box ${activeClass === 'wallet' ? 'menu-box-active' : ''} `}>
+                <img src={wallet} alt=""/>
+                <h5 onClick = {() => setActiveClass('wallet')}> <Link to="/Wallet">Wallet</Link> </h5>
+                </div>
+            
             </div>
-            <div className="menu-box">
-            <img src={market} alt=""/>
-            <h5> <Link to="/Market">Market</Link> </h5>
+            
+            <div id="market">
+                <div className={`menu-box ${activeClass === 'market'? 'menu-box-active' : ''} `}>
+                <img src={market} alt=""/>
+                <h5 onClick =  {() => setActiveClass('market') }> <Link to="/Market">Market</Link> </h5>
+                </div>
             </div>
-            <div className="menu-box">
-            <img src={earn} alt=""/>
-            <h5> <Link to="/Earn">Earn</Link> </h5>
+            
+            <div id="earn">
+                <div className={`menu-box ${activeClass === 'earn' ? 'menu-box-active' : ''} `}>
+                <img src={earn} alt=""/>
+                <h5 onClick =  {() => setActiveClass('earn')}> <Link to="/Earn">Earn</Link> </h5>
+                </div>
             </div>
+
             
         </div>
     )
